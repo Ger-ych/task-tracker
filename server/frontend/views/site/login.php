61072e9707a97c8acf312eab2492dda373803738
Ger-ych <viking-e@bk.ru>
2023-07-20 02:10:15 +0500

    feat(): Initial setup

--- server/frontend/views/site/login.php
+++ server/frontend/views/site/login.php
@@ -8,7 +8,6 @@ use yii\bootstrap4\Html;
 use yii\bootstrap4\ActiveForm;
 
 $this->title = 'Login';
-$this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-login">
     <h1><?= Html::encode($this->title) ?></h1>
