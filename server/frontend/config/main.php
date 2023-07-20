8261e194526b7878728f93c21f8af0e1a71be4fb
Ger-ych <viking-e@bk.ru>
2023-07-21 02:41:52 +0500

    Wip(): Auth controller

--- server/frontend/config/main.php
+++ server/frontend/config/main.php
@@ -14,6 +14,9 @@ return [
     'components' => [
         'request' => [
             'csrfParam' => '_csrf-frontend',
+            'parsers' => [
+                'application/json' => 'yii\web\JsonParser',
+            ]
         ],
         'user' => [
             'identityClass' => 'common\models\User',
@@ -36,14 +39,12 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
         ],
-        */
     ],
     'params' => $params,
 ];
---
+++ server/frontend/controllers/AuthController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace frontend\controllers;
+
+use common\models\User;
+use Yii;
+use yii\web\Controller;
+use yii\web\UnauthorizedHttpException;
+
+/**
+ * Auth controller
+ */
+class AuthController extends Controller
+{
+    public function beforeAction($action)
+    {
+        $this->enableCsrfValidation = false;
+        return parent::beforeAction($action);
+    }
+
+    public function actionLogin()
+    {
+        $request = Yii::$app->getRequest();
+        $username = $request->getBodyParam('username');
+        $password = $request->getBodyParam('password');
+
+        $user = User::findByUsername($username);
+        if (!$user || !$user->validatePassword($password)) {
+            throw new UnauthorizedHttpException('Неверное имя пользователя или пароль.');
+        }
+
+        // $token = Token::generateToken($user->id);
+        // $token->save();
+
+        $role = Yii::$app->authManager->getRolesByUser($user->id);
+        $roleName = reset($role)->name;
+        
+        Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
+        return [
+            // 'token' => $token->token,
+            'username' => $user->username,
+            'role' => $roleName,
+        ];
+    }
+}
