2258cb761f20a4ec79c5ef89532b1c7130bd88ac
Ger-ych <viking-e@bk.ru>
2023-08-08 22:35:23 +0500

    feat(): Added task completion functionality on my tasks page

--- client/src/components/screens/my_tasks/MyTasks.jsx
+++ client/src/components/screens/my_tasks/MyTasks.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react'
-import { useQuery } from '@tanstack/react-query';
+import React from 'react'
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 
 import { TaskService } from '../../../services/task.service';
 import { useAuth } from "../../../hooks/useAuth"
@@ -11,7 +11,23 @@ import { Link } from 'react-router-dom';
 
 const MyTasks = () => {
     const { user } = useAuth();
+
+    const queryClient = useQueryClient();
     const {data, isLoading} = useQuery(['my_tasks'], () => TaskService.getMyTasks(user.token))
+
+    const setTaskDoneMutation = useMutation((taskId) => TaskService.setTaskDone(user.token, taskId), {
+        onSuccess: () => {
+            queryClient.invalidateQueries('my_tasks');
+        },
+        onError: (err) => {
+            console.error(err.message)
+        }
+    });
+
+    const handleSetTaskDone = (taskId) => {
+        
+        setTaskDoneMutation.mutate(taskId);
+    };
 
     return (
         <main className='container'>
@@ -40,14 +56,14 @@ const MyTasks = () => {
                                 <div className="mb-1 text-body-secondary">Проект: {task.project.name}</div>
                                 <div className="mb-1 text-body-secondary">Репозиторий: <a href={task.project.repo_link}>{task.project.repo_link}</a></div>
 
-                                <div className="mt-3 accordion accordion-flush" id="accordionFlushExample">
+                                <div className="mt-3 accordion accordion-flush" id={`accordionTask${task.id}`}>
                                     <div className="accordion-item border">
                                         <h2 className="accordion-header">
-                                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target={`#flush-collapseOne${task.id}`} aria-expanded="false" aria-controls={`flush-collapseOne${task.id}`}>
                                             Подробнее
                                         </button>
                                         </h2>
-                                        <div id="flush-collapseOne" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                                        <div id={`flush-collapseOne${task.id}`} className="accordion-collapse collapse" data-bs-parent={`#accordionTask${task.id}`}>
                                             <div className="accordion-body">
                                                 <p><b>Проект ({task.project.name}):</b> {task.project.description}</p>
                                                 <p><b>Описание задачи:</b><br /> {task.text}</p>
@@ -57,7 +73,7 @@ const MyTasks = () => {
                                 </div>
                                 
                                 {!task.is_done ? (
-                                    <button className="btn btn-success w-100 mt-2">Задача выполнена <i className="fa-solid fa-check"></i></button>
+                                    <button className="btn btn-success w-100 mt-2" onClick={() => handleSetTaskDone(task.id)}>Задача выполнена <i className="fa-solid fa-check"></i></button>
                                 ) : null}
                             </div>
                         </div>
